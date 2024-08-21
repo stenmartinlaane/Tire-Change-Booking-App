3565634a7df8098c22dbd07fbf9784516b4d2e71
stlaan <stlaan@ttu.ee>
2024-08-21 18:01:10 +0300

    Initial commit

---
+++ booking-backend/src/main/java/com/stenmartin/project/booking_backend/TireChangeBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.stenmartin.project.booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireChangeBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireChangeBookingAppApplication.class, args);
+	}
+
+}
---
+++ booking-backend/src/test/java/com/stenmartin/project/booking_backend/TireChangeBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stenmartin.project.booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireChangeBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
